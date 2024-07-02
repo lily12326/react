b4d6e08f003ba8ba9bda68db9fa9854de4e1d9bd
lily12326 <64433688+lily12326@users.noreply.github.com>
2024-07-02 22:44:17 +0800

    counter app

---
+++ counter app/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  const [step, setStep] = useState(1);
+
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((c) => Math.max(c - 1, 1))}>-</button>
+        <span>Step:{step}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+ </button>
+      </div>
+      <div>
+        <button onClick={() => setCount((count) => count - step)} >- </button>
+        <span>Count:{count}</span>
+        <button onClick={() => setCount((count) => count + step)}>+ </button>
+      </div>
+      <p>
+        <span>{count === 0 ? "Today is" : count > 0 ? `${count} days from today is` : `${Math.abs(count)} days ago was`} </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div >
+  );
+}

909571b5ee8cdb7448952de8c223ed4e90a1f6ed
lily12326 <64433688+lily12326@users.noreply.github.com>
2024-07-06 18:32:43 +0800

    sorting items

--- travel list/src/components/App.js
+++ travel list/src/components/App.js
@@ -59,10 +59,29 @@ function Form({ onAddItmes }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItens }) {
+    const [sort, setSort] = useState("input");
+
+    let sortedItems;
+    if (sort === "input") sortedItems = items;
+    //slice method for not mutating the array
+    if (sort === "description") sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+    if (sort === "packed") sortedItems = items, slice().sort((a, b) => Number(a.packed) - Number(b.packed))
+
+
     return (
-        <ul className="LIST">
-            {items.map((item) => <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItens={onToggleItens} />)}
-        </ul>
+        <>
+            <ul className="LIST">
+                {items.map((item) => <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItens={onToggleItens} />)}
+            </ul>
+
+            <div>
+                <select value={sort} onChange={(e) => setSort(e.target.value)}>
+                    <option value="input">Sort By Input Order</option>
+                    <option value="desciption">Sort By Description</option>
+                    <option value="packed">Sort By Packed Status</option>
+                </select>
+            </div>
+        </>
     )
 }
 
